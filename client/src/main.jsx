c7e9a248c435ff384ff56ca5e74154077b8ea462
Yana Mishyna <yana.mishina.92@gmail.com>
2023-11-22 10:58:48 -0500

    created main and app.jsx

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -1,10 +1,37 @@
-import React from 'react'
 import ReactDOM from 'react-dom/client'
+import { createBrowserRouter, RouterProvider } from 'react-router-dom'
+import About from './components/About'
+import Portfolio from './components/Portfolio'
+import Contact from './components/Contact'
+import Resume from './components/Resume'
 import App from './App.jsx'
 import './index.css'
 
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <App />,
+    children: [
+      {
+        index: true,
+        element: <About />,
+      },
+      {
+        path: "portfolio",
+        element: <Portfolio />,
+      },
+      {
+        path: "contact",
+        element: <Contact />,
+      },
+      {
+        path: "resume",
+        element: <Resume />,
+      }
+    ]
+  }
+])
+
 ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+  <RouterProvider router={router}/>
 )
